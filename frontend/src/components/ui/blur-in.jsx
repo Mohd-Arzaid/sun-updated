c3311167c73ac44f94fb688aaf45c94e472f508b
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-17 16:08:36 +0530

    fk it

--- frontend/src/components/ui/blur-in.jsx
+++ frontend/src/components/ui/blur-in.jsx
@@ -16,7 +16,6 @@ const BlurIn = ({ word, className, variant, duration = 1 }) => {
       transition={{ duration }}
       variants={combinedVariants}
       className={cn(" text-center text-md font-bold drop-shadow-sm", className)}
-      style={{ willChange: "filter, opacity", transform: "translateZ(0)" }}
     >
       {word}
     </motion.div>
