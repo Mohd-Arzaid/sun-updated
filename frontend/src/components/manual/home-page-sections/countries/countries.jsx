47b3ce1c6ec7ac9549acda707d2afa4c76bd6cc7
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-19 16:52:31 +0530

    fk it

--- frontend/src/components/manual/home-page-sections/countries/countries.jsx
+++ frontend/src/components/manual/home-page-sections/countries/countries.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Countries = () => {
-  return <div>countries</div>;
+    return (
+      <div className="bg-white pt-8 pb-12 sm:pt-10 sm:pb-14 md:pt-12 md:pb-16 border-2 border-neutral-100">
+        <div className="">Countries We Serve</div>
+      </div>
+    );
 };
 
 export default Countries;
