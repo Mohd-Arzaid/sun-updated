2a2a8e5b0c4be88a0058f0d812aa4bc4e3f2c83f
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-19 18:08:54 +0530

    fk it

--- frontend/src/components/manual/home-page-sections/countries/countries.jsx
+++ frontend/src/components/manual/home-page-sections/countries/countries.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Countries = () => {
-    return (
-      <div className="bg-white pt-8 pb-12 sm:pt-10 sm:pb-14 md:pt-12 md:pb-16 border-2 border-neutral-100">
-        <div className="">Countries We Serve</div>
+  return (
+    <div className="bg-white pt-8 pb-12 sm:pt-10 sm:pb-14 md:pt-12 md:pb-16 border-2 border-neutral-100">
+      <div className="max-w-[84rem] mx-auto px-4 sm:px-6 md:px-12">
+        {/* Heading */}
+        <div className="text-center mb-6 sm:mb-8">
+          <p className="text-sm sm:text-base md:text-lg lg:text-xl font-geist text-neutral-600 max-w-xs sm:max-w-md md:max-w-xl mx-auto px-4 sm:px-2 md:px-0">
+            Glimpse of the Countries we have Served In, Delivering excellence
+            everywhere.
+          </p>
+        </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Countries;
