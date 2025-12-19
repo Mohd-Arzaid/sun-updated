e176e71b0fd58890f39159e19a6506c9bcd625fb
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-19 18:29:25 +0530

    fk it

--- frontend/src/components/manual/home-page-sections/latest-news-marquee/latest-news-marquee.jsx
+++ frontend/src/components/manual/home-page-sections/latest-news-marquee/latest-news-marquee.jsx
@@ -28,7 +28,7 @@ const LatestNewsMarquee = () => {
   const duplicated = [...latestNews, ...latestNews];
 
   return (
-    <div className="bg-white pt-8 pb-12 sm:pt-10 sm:pb-14 md:pt-12 md:pb-16">
+    <div className="bg-white pt-8 pb-10 sm:pt-10 sm:pb-14 md:pt-12 md:pb-14">
       <div className="max-w-[84rem] mx-auto px-4 sm:px-6 md:px-12">
         <div className="text-center mb-6 sm:mb-9">
           <h2 className="font-playfair tracking-tight text-3xl sm:text-4xl md:text-5xl font-bold drop-shadow-lg text-center text-neutral-800">
