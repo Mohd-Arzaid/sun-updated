fa9a12f2acfe7483f2c78d5f67529bc1ec540d51
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-23 13:22:04 +0530

    fk it

--- frontend/src/components/manual/home-page-sections/whats-our-customers-say/whats-our-customers-say.jsx
+++ frontend/src/components/manual/home-page-sections/whats-our-customers-say/whats-our-customers-say.jsx
@@ -143,7 +143,7 @@ const WhatsOurCustomersSay = () => {
             {duplicated.map((testimonial, i) => (
               <div
                 key={`testimonial-${testimonial.id}-${i}`}
-                className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-neutral-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] h-[280px] md:h-[320px]"
+                className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-neutral-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-6 py-4 md:px-8 md:py-6 md:w-[450px] h-[280px] md:h-[320px]"
               >
                 <p className="text-sm md:text-lg font-geist text-neutral-600 mb-4 md:mb-6">
                   {testimonial.content}
