cb28d0908bba08627070c45d2e703ebdcfe83cd4
Mohd-Arzaid <arzaid0101030@gmail.com>
2025-12-27 15:45:54 +0530

    fk it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/home/home";
 import Footer from "./components/common/footer/footer";
 import Navbar from "./components/common/navbar/navbar";
+import Topbar from "./components/common/topbar/Topbar";
 
 const App = () => {
   return (
     <>
+      <Topbar />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
