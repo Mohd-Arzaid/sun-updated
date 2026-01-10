ec8875075ef11911427c7650ca4ce0ad93f83ac7
Mohd-Arzaid <arzaid0101030@gmail.com>
2026-01-10 16:53:48 +0530

    fk it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./components/pages/home/home";
 import Footer from "./components/common/footer/footer";
 import Navbar from "./components/common/navbar/navbar";
-import Topbar from "./components/common/topbar/Topbar";
+import Topbar from "./components/common/topbar/topbar";
 import BISFMPagesRoutes from "./routes/bisfm-pages-routes";
 import OrganizationChart from "./components/pages/organization-chart/organization-chart";
 
